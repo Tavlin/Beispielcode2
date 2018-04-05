e0c5283195935d7ef73a813c16ee868cc3b569ee
Tavlin <mhemmer@stud.uni-frankfurt.de>
2018-04-05 16:16:10 +0200

    added new prog

---
+++ CommenHeader.h
@@ -0,0 +1,223 @@
+#include "TLatex.h"
+#include "TLegend.h"
+#include "TObject.h"
+#include "TCanvas.h"
+#include "TF1.h"
+#include "TStyle.h"
+#include "TString.h"
+#include "TMath.h"
+#include "TGaxis.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH1D.h"
+#include "TH2F.h"
+#include "TTree.h"
+#include "TRandom.h"
+#include <TSystem.h>
+#include <iostream>
+
+const Int_t kMaxHit = 2000;
+
+
+Float_t fCalcInvMass(Float_t px1, Float_t py1, Float_t pz1, Float_t px2, Float_t py2, Float_t pz2){
+  //cout << "ERROR: Die Funktion fCalcInvMass muss erst noch geschrieben werden (Press Strg+C)" << endl;
+  return 0;
+}
+
+Float_t fCalcPT(Float_t px1, Float_t py1, Float_t px2, Float_t py2){
+  //cout << "ERROR: Die Funktion fCalcPT muss erst noch geschrieben werden (Press Strg+C)" << endl;
+  return 0;
+}
+
+
+void RotateToLabSystem(const float& theta, const float& phi,
+		       const float& p1, const float& p2, const float& p3,
+		       float& p1rot, float& p2rot, float& p3rot) {
+
+  Float_t st = sin(theta);
+  Float_t ct = cos(theta);
+  Float_t sp = sin(phi);
+  Float_t cp = cos(phi);
+
+  p1rot = cp*ct*p1 - p2*sp + cp*p3*st;
+  p2rot = cp*p2 + ct*p1*sp + p3*sp*st;
+  p3rot = ct*p3 - p1*st;
+
+}
+
+
+class DataTree{
+  private:
+    Float_t pxdata[kMaxHit];
+    Float_t pydata[kMaxHit];
+    Float_t pzdata[kMaxHit];
+    Int_t iNCluster;
+    Int_t NEvents;
+    TTree* tClusters;
+
+  public:
+    DataTree(TFile* fDaten){
+      tClusters = (TTree*) fDaten->Get("ntu");
+      NEvents = tClusters->GetEntries();
+      tClusters->SetBranchAddress("nhit", &iNCluster);
+      tClusters->SetBranchAddress("px", pxdata);
+      tClusters->SetBranchAddress("py", pydata);
+      tClusters->SetBranchAddress("pz", pzdata);
+    }
+
+    void GetEvent(Int_t iEvt){
+      tClusters->GetEntry(iEvt);
+    }
+
+    Float_t GetPX(Int_t iEvt, Int_t iHit){
+      GetEvent(iEvt);
+      return pxdata[iHit];
+    }
+
+    Float_t GetPY(Int_t iEvt, Int_t iHit){
+      GetEvent(iEvt);
+      return pydata[iHit];
+    }
+
+    Float_t GetPZ(Int_t iEvt, Int_t iHit){
+      GetEvent(iEvt);
+      return pzdata[iHit];
+    }
+    Float_t GetClusterID(Int_t iEvt){
+      GetEvent(iEvt);
+      return iNCluster;
+    }
+
+    Int_t GetNEvents(){
+      return NEvents;
+    }
+};
+
+
+
+
+void SetCanvasStandardSettings(TCanvas *cCanv){
+  // gStyle->SetOptStat(0); // <- das hier macht dies box rechts oben weg
+  cCanv->SetTopMargin(0.025);
+  cCanv->SetBottomMargin(0.15);
+  cCanv->SetRightMargin(0.05);
+  cCanv->SetLeftMargin(0.15);
+  cCanv->SetTickx();
+  cCanv->SetTicky();
+  cCanv->SetLogy(0);
+  cCanv->SetLogx(0);
+}
+
+
+void SetHistoStandardSettings(TH1* histo, Double_t XOffset = 1.2, Double_t YOffset = 1.){
+  histo->GetXaxis()->SetTitleOffset(XOffset);
+  histo->GetYaxis()->SetTitleOffset(YOffset);
+  histo->GetXaxis()->SetTitleSize(45);
+  histo->GetYaxis()->SetTitleSize(45);
+  histo->GetXaxis()->SetLabelSize(45);
+  histo->GetYaxis()->SetLabelSize(45);
+  histo->GetXaxis()->SetLabelFont(43);
+  histo->GetYaxis()->SetLabelFont(43);
+  histo->GetYaxis()->SetTitleFont(43);
+  histo->GetXaxis()->SetTitleFont(43);
+
+
+  histo->SetMarkerStyle(20);
+  histo->SetMarkerSize(1.5);
+  histo->SetLineWidth(1.5);
+  histo->SetLineColor(kBlack);
+  histo->SetMarkerColor(kBlack);
+}
+
+void SetHistoStandardSettings2(TH2* histo, Double_t XOffset = 1.2, Double_t YOffset = 1.){
+  histo->GetXaxis()->SetTitleOffset(XOffset);
+  histo->GetYaxis()->SetTitleOffset(YOffset);
+  histo->GetXaxis()->SetTitleSize(45);
+  histo->GetYaxis()->SetTitleSize(45);
+  histo->GetXaxis()->SetLabelSize(45);
+  histo->GetYaxis()->SetLabelSize(45);
+  histo->GetXaxis()->SetLabelFont(43);
+  histo->GetYaxis()->SetLabelFont(43);
+  histo->GetYaxis()->SetTitleFont(43);
+  histo->GetXaxis()->SetTitleFont(43);
+}
+
+
+void SetLegendSettigns(TLegend* leg){
+  leg->SetTextSize(50);
+  leg->SetTextFont(43);
+  leg->SetFillColor(0);
+  leg->SetFillStyle(0);
+  leg->SetLineWidth(0);
+  leg->SetLineColor(0);
+  leg->SetMargin(0.15);
+}
+
+
+
+// gStyle->SetCanvasColor(0);
+// gStyle->SetPadColor(0);
+// gStyle->SetCanvasBorderMode(0);
+// gStyle->SetPadBorderMode(0);
+//
+// gStyle->SetTitleXOffset(1.4);
+// gStyle->SetTitleYOffset(1.8);
+//
+// gStyle->SetPadLeftMargin(0.17);
+// gStyle->SetPadRightMargin(0.1);      // 0.1 = root default
+// gStyle->SetPadTopMargin(0.1);
+// gStyle->SetPadBottomMargin(0.14);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// Advanced
+
+void DrawLabelALICE(Double_t startTextX         = 0.13, Double_t startTextY         = 0.9, Double_t textHeight         = 0.02, Double_t textSize         = 40){
+  TString textAlice       = "ALICE work in progress";
+  TString textEvents      = "Data";
+
+  Double_t differenceText     = textHeight*1.5;
+  TLatex *alice               = new TLatex(startTextX, startTextY, Form("%s",textAlice.Data()));
+  TLatex *energy             = new TLatex(startTextX, (startTextY-1.*differenceText), "pp, #sqrt{s} = 5.02 TeV, 113M events");
+
+  TLatex *detprocess          = new TLatex(startTextX, (startTextY-2.*differenceText), "#pi^{0}#rightarrow#gamma#gamma, #gamma's rec. with DCal ");
+
+  // TLatex *pt          = new TLatex(startTextX, (startTextY-1*differenceText), Form("%3.1f GeV/#it{c} #leq %s < %3.1f GeV/#it{c}",startPt,ptLabel.Data(),endPt));
+
+  alice->SetNDC();
+  alice->SetTextColor(1);
+  alice->SetTextFont(43);
+  alice->SetTextSize(textSize*1.3);
+  alice->Draw();
+
+  energy->SetNDC();
+  energy->SetTextColor(1);
+  energy->SetTextSize(textSize);
+  energy->SetTextFont(43);
+  energy->Draw();
+
+
+  detprocess->SetNDC();
+  detprocess->SetTextColor(1);
+  detprocess->SetTextSize(textSize);
+  detprocess->SetTextFont(43);
+  detprocess->Draw();
+
+  // pt->SetNDC();
+  // pt->SetTextColor(1);
+  // pt->SetTextSize(textSize);
+  // pt->SetTextFont(43);
+  // pt->Draw();
+}
---
+++ Reconstruction.C
@@ -0,0 +1,112 @@
+#include "CommenHeader.h"
+
+void Reconstruction() {
+  // Wir definieren ein Canvas auf das wir malen können
+  TCanvas *cExtractSignal = new TCanvas("cExtractSignal","",800,800);
+  // Wir stellen ein paar grundlegende Settings ein
+  SetCanvasStandardSettings(cExtractSignal);// (diese Funktion ist in ExtractSignal.h definiert)
+
+  TH1F* hSignal = new TH1F("hSignal","invariante Masse",100,0.,0.3);
+  SetHistoStandardSettings(hSignal);
+  TH1F* hSignalmix = new TH1F("hSignalmix","invariante Masse (mixed events)",100,0.,0.3);
+  SetHistoStandardSettings(hSignalmix);
+  TH2F* hSignal_pT = new TH2F("hSignal_pT","invariante Masse gegen pT",100,0.,0.3,20,0.,10.);
+  SetHistoStandardSettings2(hSignal_pT);
+  TH2F* hSignalmix_pT = new TH2F("hSignalmix_pT","invariante Masse gegen pT (mixed events)",100,0.,0.3,20,0.,10.);
+  SetHistoStandardSettings2(hSignalmix_pT);
+
+  // read Cluster Tree
+  TFile* fDaten = new TFile("pi0_vcal_data.root");
+
+
+  const Int_t iPufferMax = 2;
+  Int_t NMaxEvents = 10;
+
+
+  if(fDaten->IsZombie()){
+    cout << "ERROR: Data File not found" << endl;
+    return;
+  }
+  DataTree *Daten = new DataTree(fDaten);
+  // NMaxEvents = Daten->GetNEvents()
+
+  // Allokiere Arrays in die wir die Cluster schreiben
+  Float_t px[iPufferMax][kMaxHit];
+  Float_t py[iPufferMax][kMaxHit];
+  Float_t pz[iPufferMax][kMaxHit];
+  Float_t iCluster[iPufferMax];
+  Int_t iNCluster;
+
+  // Setze Anzahl der Cluster in jedem Puffer auf null
+  for (Int_t i = 0; i < iPufferMax; i++) {
+    iCluster[i] = 0;
+  }
+  int iPufferAktuell = 0;
+
+  for (Int_t iEvt=0; iEvt < NMaxEvents; iEvt++) {
+    if(iEvt%1000 == 0 && iEvt !=0 ) cout << iEvt/1000 << "x10^3 Events have been analyzed" << endl;
+
+    // Anzahl Cluster im Event
+    iNCluster = Daten->GetClusterID(iEvt);
+
+    for (Int_t iHit=0; iHit < iNCluster; iHit++) {
+      px[iPufferAktuell][iHit] = Daten->GetPX(iEvt, iHit);
+      py[iPufferAktuell][iHit] = Daten->GetPY(iEvt, iHit);
+      pz[iPufferAktuell][iHit] = Daten->GetPZ(iEvt, iHit);
+
+    }
+    iCluster[iPufferAktuell] = iNCluster;
+
+    // starte Analyse des letzten Events
+    Float_t pair_pt;
+    Float_t minv;
+    Float_t px1;
+    Float_t py1;
+    Float_t pz1;
+    Float_t px2;
+    Float_t py2;
+    Float_t pz2;
+
+    for (int i1 = 0; i1 < iCluster[iPufferAktuell]; i1++) {
+
+      // Paare im selben Event
+      // ....
+
+
+
+      // Paare aus unterschiedlichen Events (Event Mixing)
+      int iPufferAlt;
+      if (iPufferAktuell == 0) {
+	       iPufferAlt = 1;
+      } else {
+	       iPufferAlt = 0;
+      }
+      for (int i2 = 0; i2 < iCluster[iPufferAlt]; i2++) {
+  	     px1 = px[iPufferAktuell][i1];
+  	     py1 = py[iPufferAktuell][i1];
+  	     pz1 = pz[iPufferAktuell][i1];
+  	     px2 = px[iPufferAlt][i2];
+  	     py2 = py[iPufferAlt][i2];
+  	     pz2 = pz[iPufferAlt][i2];
+
+         pair_pt = fCalcPT(px1,py1,px2,py2);
+      	 if (pair_pt > 0) {
+      	    minv = fCalcInvMass(px1,py1,pz1,px2,py2,pz2);
+      	 }
+
+         // Fülle Histogramme
+         // ....
+
+      }
+    }
+
+    // Bereite naechstes Event vor
+
+    // Puffer umschalten
+    iPufferAktuell++;
+    if (iPufferAktuell == iPufferMax) {
+      iPufferAktuell = 0;
+    }
+
+  }
+}
